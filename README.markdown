fe402c882d97b9967e13e16c35a6d2f3df729f95
Jeff Keen <jeff@keen.me>
2016-05-12 13:37:32 -0500

    First revision of forever chat format.

---
+++ README.markdown
@@ -0,0 +1,60 @@
+##Forever Chat Format
+
+##Why
+
+Because in the world where our communication is siloed into Google Hangouts, Facebook, iMessage, WhatsApp, etc etc it's getting hard to hold onto conversations for archival purposes. First thing we need is a universal format that we can export those proprietary chats into that's easy to understand, and that can be relied upon.
+
+##Basic Format
+
+Output should be an array of hashes, and each object in the array is a JSON object defined as follows:
+
+##Chat Object Definition:
+
+Name              | Type              | Description
+-----------       | -------           |   -------------
+sha               | string            | sha1 of [address, date, text, service]
+sender            | string            | The handle of the person sending the message
+receiver          | string            | The handle of the person receiving the message
+is_from_me        | boolean           | true if you sent the message
+date              | ISO-8601 datetime | date message was sent/received by sender
+service           | string            | Service message was sent using. All lowercase, dasherized. 'imessage', 'google-hangouts', 'facebook-messenger', 'sms'
+date_read         | ISO-8601 datetime | (optional) date message was sent/read by receiver
+date_delivered    | ISO-8601 datetime | (optional) date message was delivered by receiver
+participants      | array             | (see definition below) An array of strings of handles involved in the conversations. Used for grouping. For a two person conversation, this has two entries in it. Sender and receiver are always here.
+message_text      | string            | text representation of the message
+message_segments  | array             | (see definition below) ordered segments of content as it appears in the message
+attachments       | array             | (see definition below) array of attachments
+
+
+####Message Segment Definition
+
+Name        |  Type      |  Possible Values    | Description
+----------- |  -------   |  -----------------  | -------------
+type        | string     |  text, link, file   | base segment type
+file_type   | string     |  null, or a mime_type | only valid when type == file
+path        | string     |  url or file path  |  
+text        | string     |                    | the rendered name of the link, or the text content for the text, or optionally
+
+
+##### Text Segment
+```json
+{"type": "text", "text": "Why hello there"}
+```
+##### Link Segment
+```json
+{"type": "link", "text": "Whoa, unreal", "path": "http://latlmes.com/technology/why-we-need-a-new-chat-format-1"}
+```
+##### File Segment
+```json
+{"type": "file", "file_type": "image/png", "path": "/path/to/image.png"}
+```
+
+
+##Tests:
+The included tests check whether some json is valid forever chat format.
+```javascript
+var foreverJson    = importer("test.json"); // done by an exporter
+
+var formatTests    = require('forever-chat-format-tests');
+formatTests(foreverJson);
+```
---
+++ index.js
@@ -0,0 +1,122 @@
+var mocha  = require('mocha');
+var chai   = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var assert = chai.assert;
+
+var _ = require('underscore');
+
+function findInvalidDates(dates) {
+  var badDates = [];
+  _.each(dates, function(d) {
+    if (d.match(/^\d\d\d\d-\d\d-\d\dT\d\d\:\d\d\:\d\d/).length > 0) {
+      // good
+    }
+    else {
+      badDates.push(d);
+    }
+  });
+
+  return badDates;
+}
+
+function runTests(promiseOrData) {
+  // Give this a promise or data and it'll run through the tests to figure out
+  // if the data conforms with the universal chat format
+
+  describe('conforms to universal chat format V 1.0', function() {
+    before(function(done) {
+      this.timeout(30000);
+
+      if ('function' === typeof promiseOrData.then) {
+        return promiseOrData.then(function(d) {
+          data = d;
+          done();
+        }, function(reason) {
+          done();
+        });
+      } else {
+          data = promiseOrData;
+      }
+    });
+
+    it('should return lots of chats', function() {
+      return expect(data.length).to.be.greaterThan(1);
+    });
+
+    it('each item has a unique sha', function() {
+      var shaMap = {};
+      var duplicates = [];
+
+      var shas = _.map(data, function(d) {
+        var s = JSON.parse(d).sha;
+        if (shaMap[s]) {
+          duplicates.push(d);
+          duplicates.push(shaMap[s]);
+        }
+        shaMap[s] = d;
+        return s;
+      });
+
+      var uniques = _.unique(shas);
+      expect(shas.length).to.be.equal(data.length, "each message should have a sha");
+      expect(duplicates.length).to.be.equal(0, "each sha should be unique " + JSON.stringify(duplicates));
+    });
+
+    it('each date is set properly and in ISO-8601 format', function() {
+      var dates = _.map(data, function(d) {
+        return JSON.parse(d).date;
+      });
+
+      expect(dates.length).to.be.equal(data.length);
+      expect(findInvalidDates(dates).length).to.be.equal(0);
+    });
+
+    it('each date_read is set properly and in ISO-8601 format', function() {
+      var dates = _.compact(_.map(data, function(d) {
+        return JSON.parse(d).date_read;
+      }));
+
+      expect(findInvalidDates(dates).length).to.be.equal(0);
+    });
+
+    it('each date_delivered is set properly and in ISO-8601 format', function() {
+      var dates = _.compact(_.map(data, function(d) {
+        return JSON.parse(d).date_delivered;
+      }));
+
+      expect(findInvalidDates(dates).length).to.be.equal(0);
+    });
+
+    it('each date_read is set properly and in ISO-8601 format', function() {
+      var dates = _.compact(_.map(data, function(d) {
+        return JSON.parse(d).date_read;
+      }));
+
+      expect(findInvalidDates(dates).length).to.be.equal(0);
+    });
+
+    it('attachment array is a thing', function() {
+      var attachments = _.map(data, function(d) {
+        return JSON.parse(d).attachments;
+      });
+
+      _.each(attachments, function(d) {
+        expect(d).to.be.a('array');
+      });
+    });
+
+    it('participants always includes sender and receiver', function() {
+      return _.each(data, function(d) {
+        var message = JSON.parse(d);
+        expect(message.participants).to.contain(message.sender);
+        _.each(message.receiver, function(r) {
+          expect(message.participants).to.contain(r);
+        });
+      });
+    });
+  });
+}
+
+module.exports = runTests;
